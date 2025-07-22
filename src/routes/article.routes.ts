734c6c0ca5de2d99ada847f0ca5186f0595a99c4
shoshana5370 <shg6745370@gmail.com>
2025-07-22 13:18:00 +0300

    update in schema

--- src/routes/article.routes.ts
+++ src/routes/article.routes.ts
@@ -2,14 +2,14 @@ import { Router } from 'express';
 import * as articleController from '../controllers/article.controller';
 import { validateIdParam } from '../middlewares/validateIdParam.middleware';
 import validateBody from '../middlewares/validateBody.middeleware';
-import { articleSchema } from '../schemas/article.schema';
+import { createArticleSchema, updateArticleSchema } from '../schemas/article.schema';
 
 const router = Router();
 router.get('/', articleController.getAllArticales);
 router.get('/by-category/:categoryId', articleController.getByCategory);
 router.post('/', 
-    validateBody(articleSchema),
+    validateBody(createArticleSchema),
     articleController.createArtical);
 router.delete('/:id',validateIdParam, articleController.deleteArtical);
-router.put('/:id', validateIdParam, validateBody(articleSchema), articleController.updateArtical);
+router.put('/:id', validateIdParam, validateBody(updateArticleSchema), articleController.updateArtical);
 export default router;
--- src/services/article.service.ts
+++ src/services/article.service.ts
@@ -21,9 +21,14 @@ export const createArtical = (data: Omit<article, "createdAt">) => {
 };
 
 export const updateArtical = (id: number, data: Partial<article>) => {
-  return prisma.article.update({ where: { id }, data });
+  return prisma.article.update({
+    where: { id },
+    data: {
+      ...data,
+      updatedAt: new Date()
+    }
+  });
 };
-
 export const deleteArtical = (id: number) => {
   return prisma.article.delete({ where: { id } });
 };
