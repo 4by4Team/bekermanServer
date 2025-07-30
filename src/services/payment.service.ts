8acbae6cbff9ee482692233caf288c8ce074f80f
shoshana5370 <shg6745370@gmail.com>
2025-07-31 02:04:08 +0300

    add env

--- src/services/payment.service.ts
+++ src/services/payment.service.ts
@@ -3,71 +3,76 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PAYPAL_API_URL = "https://api-m.sandbox.paypal.com";
+const PAYPAL_API_URL = process.env.PAYPAL_API || "https://api-m.sandbox.paypal.com"; // Use sandbox for testing
 const CLIENT_ID = process.env.PAYPAL_CLIENT_ID!;
 const CLIENT_SECRET = process.env.PAYPAL_CLIENT_SECRET!;
 
 // פונקציה ליצירת Access Token
 async function generateAccessToken(): Promise<string> {
-  const auth = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString("base64");
-  const response = await axios.post(
-    `${PAYPAL_API_URL}/v1/oauth2/token`,
-    "grant_type=client_credentials",
-    {
-      headers: {
-        Authorization: `Basic ${auth}`,
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    }
-  );
 
-  return response.data.access_token;
+    const auth = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString("base64");
+    const response = await axios.post(
+        `${PAYPAL_API_URL}/v1/oauth2/token`,
+        "grant_type=client_credentials",
+        {
+            headers: {
+                Authorization: `Basic ${auth}`,
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+        }
+    );
+
+    return response.data.access_token;
 }
 
 // שירות להפקת הזמנה
 export async function createOrder(): Promise<any> {
-  try {
-    const accessToken = await generateAccessToken();
-    const response = await axios.post(
-      `${PAYPAL_API_URL}/v2/checkout/orders`,
-      {
-        intent: "CAPTURE",
-        purchase_units: [
-          {
-            amount: {
-              currency_code: "USD",
-              value: "10.00", // סכום התשלום
+    try {
+        console.log(CLIENT_ID, CLIENT_SECRET);
+        
+        const accessToken = await generateAccessToken();
+        const response = await axios.post(
+            `${PAYPAL_API_URL}/v2/checkout/orders`,
+            {
+                intent: "CAPTURE",
+                purchase_units: [
+                    {
+                        amount: {
+                            currency_code: "USD",
+                            value: "10.00", // סכום התשלום
+                        },
+                    },
+                ],
             },
-          },
-        ],
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
-    return response.data;
-  } catch (error) {
-    throw new Error("Error creating order with PayPal");
-  }
+            {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            }
+        );
+        return response.data;
+    } catch (error: any) {
+        console.error("PayPal error:", error?.response?.data || error.message);
+        throw new Error("Error creating order with PayPal");
+
+    }
 }
 
 // שירות לאישור תשלום
 export async function captureOrder(orderID: string): Promise<any> {
-  try {
-    const accessToken = await generateAccessToken();
-    const response = await axios.post(
-      `${PAYPAL_API_URL}/v2/checkout/orders/${orderID}/capture`,
-      {},
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
-    return response.data;
-  } catch (error) {
-    throw new Error("Error capturing order with PayPal");
-  }
+    try {
+        const accessToken = await generateAccessToken();
+        const response = await axios.post(
+            `${PAYPAL_API_URL}/v2/checkout/orders/${orderID}/capture`,
+            {},
+            {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            }
+        );
+        return response.data;
+    } catch (error) {
+        throw new Error("Error capturing order with PayPal");
+    }
 }
